f572a78bf90a1bb36fd35e78b5fe688848a5ec1b
mademao <ismademao@gmail.com>
2019-01-08 20:26:41 +0800

    PropertySetterKidRule支持检测.h文件

--- YBQPropertySetterKidRule.cpp
+++ YBQPropertySetterKidRule.cpp
@@ -23,8 +23,63 @@ public:
         return "云表情自定义";
     }
     
-    virtual unsigned int supportedLanguages() const override{
+    virtual unsigned int supportedLanguages() const override
+    {
         return LANG_OBJC;
+    }
+    
+    virtual void apply() override
+    {
+        if (!isLanguageSupported())
+        {
+            return;
+        }
+        
+        setUp();
+        clang::DeclContext *decl = _carrier->getTranslationUnitDecl();
+        for (clang::DeclContext::decl_iterator it = decl->decls_begin(), declEnd = decl->decls_end();
+             it != declEnd; ++it)
+        {
+            
+            if (isNeedAnalysisDecl(it))
+            {
+                clang::RecursiveASTVisitor<YBQPropertySetterKidRule>::TraverseDecl(*it);
+            }
+        }
+        tearDown();
+    }
+    
+    //判断是否需要检测该节点
+    bool isNeedAnalysisDecl(clang::DeclContext::decl_iterator it)
+    {
+        clang::SourceLocation startLocation = (*it)->getLocStart();
+        if (startLocation.isInvalid())
+            return false;
+        
+        // decl 就是在当前分析文件中
+        clang::SourceManager *sourceManager = &_carrier->getSourceManager();
+        if (sourceManager->getMainFileID() == sourceManager->getFileID(startLocation))
+            return true;
+        
+        //本decl是否是在被分析文件的头文件中
+        llvm::StringRef sourceFileName = sourceManager->getFilename(startLocation);
+        if (isAnalyzeFileHeadFile(sourceFileName))
+            return true;
+        
+        return false;
+    }
+    
+    // 判断文件是否是当前分析文件的头文件
+    bool isAnalyzeFileHeadFile(llvm::StringRef fileNameStrRef) {
+        if (fileNameStrRef.empty())
+            return false;
+        
+        std::string currentAnalyzeFilePath = _carrier->getMainFilePath().c_str();
+        std::string subString = currentAnalyzeFilePath.erase(currentAnalyzeFilePath.length()-1);
+        if (fileNameStrRef.str().find(subString) == 0)
+            return true;
+        
+        return false;
     }
     
     bool VisitObjCPropertyDecl(ObjCPropertyDecl *node)
@@ -37,7 +92,8 @@ public:
         
         if (isBlock == true) {
             if ((attributeKind & ObjCPropertyDecl::PropertyAttributeKind::OBJC_PR_strong) == ObjCPropertyDecl::PropertyAttributeKind::OBJC_PR_strong) {
-                addViolation(node, this, "Block属性 "+name+" 使用copy修饰比使用strong修饰更能表达内存操作方式");
+                //在strong修饰block时，具有从栈区拷贝至堆区的功能
+//                addViolation(node, this, "Block属性 "+name+" 使用copy修饰比使用strong修饰更能表达内存操作方式");
             } else if ((attributeKind & ObjCPropertyDecl::PropertyAttributeKind::OBJC_PR_copy) != ObjCPropertyDecl::PropertyAttributeKind::OBJC_PR_copy) {
                 addViolation(node, this, "Block属性 "+name+" 应当使用copy修饰");
             }
